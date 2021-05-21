be019894caf2c8655e3c40998e1562f9ad84b774
Jaime Tarazona <jaime130798@gmail.com>
2021-05-21 04:45:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const path = require("path");
 const i18n = require("i18n");
 
 const client = new Client({
-  disableMentions: "everyone",
-  restTimeOffset: 0
+    disableMentions: "everyone",
+    restTimeOffset: 0
 });
 
 client.login(TOKEN);
@@ -21,36 +21,38 @@ const cooldowns = new Collection();
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 i18n.configure({
-  locales: ["ar", "de", "en", "es", "fr", "it", "ko", "nl", "pl", "pt_br", "ru", "sv", "tr", "zh_cn", "zh_tw"],
-  directory: path.join(__dirname, "locales"),
-  defaultLocale: "en",
-  objectNotation: true,
-  register: global,
+    locales: ["ar", "de", "en", "es", "fr", "it", "ko", "nl", "pl", "pt_br", "ru", "sv", "tr", "zh_cn", "zh_tw"],
+    directory: path.join(__dirname, "locales"),
+    defaultLocale: "en",
+    objectNotation: true,
+    register: global,
 
-  logWarnFn: function (msg) {
-    console.log("warn", msg);
-  },
+    logWarnFn: function(msg) {
+        console.log("warn", msg);
+    },
 
-  logErrorFn: function (msg) {
-    console.log("error", msg);
-  },
+    logErrorFn: function(msg) {
+        console.log("error", msg);
+    },
 
-  missingKeyFn: function (locale, value) {
-    return value;
-  },
+    missingKeyFn: function(locale, value) {
+        return value;
+    },
 
-  mustacheConfig: {
-    tags: ["{{", "}}"],
-    disable: false
-  }
+    mustacheConfig: {
+        tags: ["{{", "}}"],
+        disable: false
+    }
 });
 
 /**
  * Client Events
  */
 client.on("ready", () => {
-  console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+    console.log(`${client.user.username} ready!`);
+    client.user.setActivity(`imok 24/7 LIVE RADIO
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  COMANDOS: ⠀⠀${PREFIX}help ⠀Y ⠀${PREFIX}play `, { type: "LISTENING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
@@ -60,54 +62,54 @@ client.on("error", console.error);
  */
 const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
-  const command = require(join(__dirname, "commands", `${file}`));
-  client.commands.set(command.name, command);
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
 }
 
-client.on("message", async (message) => {
-  if (message.author.bot) return;
-  if (!message.guild) return;
+client.on("message", async(message) => {
+    if (message.author.bot) return;
+    if (!message.guild) return;
 
-  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`);
-  if (!prefixRegex.test(message.content)) return;
+    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`);
+    if (!prefixRegex.test(message.content)) return;
 
-  const [, matchedPrefix] = message.content.match(prefixRegex);
+    const [, matchedPrefix] = message.content.match(prefixRegex);
 
-  const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-  const command =
-    client.commands.get(commandName) ||
-    client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+    const command =
+        client.commands.get(commandName) ||
+        client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 
-  if (!command) return;
+    if (!command) return;
 
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Collection());
-  }
-
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 1) * 1000;
-
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(
-        i18n.__mf("common.cooldownMessage", { time: timeLeft.toFixed(1), name: command.name })
-      );
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Collection());
     }
-  }
 
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 1) * 1000;
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply(i18n.__("common.errorCommend")).catch(console.error);
-  }
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(
+                i18n.__mf("common.cooldownMessage", { time: timeLeft.toFixed(1), name: command.name })
+            );
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply(i18n.__("common.errorCommend")).catch(console.error);
+    }
 });
