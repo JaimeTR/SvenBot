716d4d4903c67281ec1521036e0ebace80ce99e5
Jaime Tarazona <38092883+JaimeTR@users.noreply.github.com>
2021-05-21 04:00:28 -0500

    Version 1.0

---
+++ commands/help.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const { LOCALE } = require("../util/EvobotUtil");
+const i18n = require("i18n");
+
+i18n.setLocale(LOCALE);
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: i18n.__("help.description"),
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
+      .setDescription(i18n.__("help.embedDescription"))
+      .setColor("#F8AA2A");
+
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
